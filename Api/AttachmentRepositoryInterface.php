e8c9defcc2ef7ccb1043d96d8a348d2e29c18f0c
Bartosz Kubicki <bartosz.kubicki@lizardmedia.pl>
2019-11-22 15:00:23 +0100

    Fix for module installation with db prefixes
    Fixing process + removal the only attachement using product form + url-type attachment download
    
    Conflicts:
    	README.md
    	composer.json

--- Api/AttachmentRepositoryInterface.php
+++ Api/AttachmentRepositoryInterface.php
@@ -11,10 +11,11 @@ declare(strict_types = 1);
 
 namespace LizardMedia\ProductAttachment\Api;
 
-use \LizardMedia\ProductAttachment\Api\Data\AttachmentInterface;
-use \Magento\Catalog\Api\Data\ProductInterface;
-use \Magento\Framework\Api\SearchCriteriaInterface;
-use \Magento\Framework\Api\SearchResultsInterface;
+use LizardMedia\ProductAttachment\Api\Data\AttachmentInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Api\SearchResultsInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 /**
  * Interface AttachmentRepositoryInterface
@@ -24,44 +25,34 @@ interface AttachmentRepositoryInterface
 {
     /**
      * @param int $id
-     *
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
-     *
-     * @return \LizardMedia\ProductAttachment\Api\Data\AttachmentInterface
+     * @return AttachmentInterface
+     * @throws NoSuchEntityException
      */
     public function getById($id) : AttachmentInterface;
 
-
     /**
-     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @param SearchCriteriaInterface $searchCriteria
      * @param int $storeId
-     *
-     * @return \Magento\Framework\Api\SearchResultsInterface
+     * @return SearchResultsInterface
      */
     public function getList(SearchCriteriaInterface $searchCriteria, int $storeId = 0) : SearchResultsInterface;
 
-
     /**
-     * @param \Magento\Catalog\Api\Data\ProductInterface $product
-     *
-     * @return \LizardMedia\ProductAttachment\Api\Data\AttachmentInterface[]
+     * @param ProductInterface $product
+     * @return AttachmentInterface[]
      */
     public function getAttachmentsByProduct(ProductInterface $product) : array;
 
-
     /**
      * @param string $sku
-     * @param \LizardMedia\ProductAttachment\Api\Data\AttachmentInterface $attachment
+     * @param AttachmentInterface $attachment
      * @param bool $isGlobalScopeContent
-     *
      * @return int
      */
-    public function save(string $sku, AttachmentInterface $attachment, bool $isGlobalScopeContent = true) : int;
-
+    public function save(string $sku, AttachmentInterface $attachment, bool $isGlobalScopeContent = true): int;
 
     /**
      * @param int $id
-     *
      * @return bool
      */
     public function delete(int $id) : bool;
---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+### 1.0.1 ###
+* making possible installing module when db tables' prefixes are used
+* fix for attachment removal, when the only attachment is being deleted using product form
+* fix for url-type attachment download
